11eec7157769dc11ad2eb6685828d86e24cde2f9
Dion Hobdy <devdionhobdy@gmail.com>
2024-12-27 23:08:40 -0800

    Update mount_drives.sh

--- setup_scripts/mount_drives.sh
+++ setup_scripts/mount_drives.sh
@@ -10,21 +10,24 @@ echo "
 | ||_|| ||       ||       || | |   |  |   |   _____  |       ||   |  | ||   |  |     | |   |___  _____| |
 |_|   |_||_______||_______||_|  |__|  |___|  |_____| |______| |___|  |_||___|   |___|  |_______||_______|
 "
-echo.
+echo
 echo "Script by Dion Hobdy"
-echo.
+echo
 # List the available drives for the user to select.
 lsblk
 
 # While loop used with the exit condition of user not needing to mount a drive.
 while true; do
+    echo
     # Ask the user if there is a drive they would like to mount.
     read -p "Mount a drive? [y/n] " yn
+    echo
     # Read for a Y or N query from user.
     case $yn in
         # If yes, ask the user for drive name and desired location.
         [Yy]* ) read -p "Enter name of drive: " drive;
                 read -p "Enter the folder location: " location;
+                echo
                 sudo mount /dev/$drive location; break;;
         # If no, state that user is exiting script and thank them for using Mount_Drives.
         [Nn]* ) echo "Exiting. Thank you for using Mount_Drives!"; exit;;
